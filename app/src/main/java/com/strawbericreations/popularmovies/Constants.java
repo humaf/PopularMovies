16f0f1341a6fe789342ab247af42cfee5257328e
humaf <qfhuma@gmail.com>
2017-08-07 22:26:31 -0700

    Added Changes asked by the reveiwer

--- app/src/main/java/com/strawbericreations/popularmovies/Constants.java
+++ app/src/main/java/com/strawbericreations/popularmovies/Constants.java
@@ -5,7 +5,7 @@ package com.strawbericreations.popularmovies;
  */
 public class Constants {
 
-    static String API_KEY="your api key";
-    static String API_URL_POP="http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=";
-    static String API_URL_TOP="http://api.themoviedb.org/3/discover/movie?sort_by=vote_average.desc&api_key=";
+    static String API_KEY="34de1fb55076a771087c2c04d80637f2";
+    static String API_URL_POP="http://api.themoviedb.org/3/movie/popular?api_key=";
+    static String API_URL_TOP="http://api.themoviedb.org/3/movie/top_rated?api_key=";
 }
